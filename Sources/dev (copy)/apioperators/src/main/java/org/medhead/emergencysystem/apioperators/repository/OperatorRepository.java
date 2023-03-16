a32e073296783a2cd8d82aba003e65416bca9dc7
solarhis <gerald.attard@orange.fr>
2023-03-16 14:20:08 +0100

    delete Sources/dev (copy)

--- Sources/dev (copy)/apioperators/src/main/java/org/medhead/emergencysystem/apioperators/repository/OperatorRepository.java
+++
@@ -1,9 +0,0 @@
-package org.medhead.emergencysystem.apioperators.repository;
-
-import org.medhead.emergencysystem.apioperators.model.Operator;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OperatorRepository extends CrudRepository<Operator, Long> {
-}
